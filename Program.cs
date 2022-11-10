6bb01236442854f29feb714897f6033f29c8a29c
RahulAid <raccess97@gmail.com>
2022-11-10 08:12:52 +0530

    Push To Create A Stack

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructures_Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+
+            Stack stk = new Stack();
+            stk.Push(56);
+            stk.Push(30);
+            stk.Push(70);
+            stk.Display(); 
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Stack_Queue
+{
+    internal class Stack
+    {
+        public Node head;
+
+        public Stack()
+        {
+            this.head = null;
+        }
+
+        public void Push(int data)
+        {
+            Node objNode = new Node(data);
+            if (head == null)
+            {
+                objNode.next = null;
+            }
+            else
+            {
+                objNode.next = head;
+            }
+            head = objNode;
+            Console.WriteLine($"{data} pushed to Stack");
+        }
+
+        public void Display()
+        {
+            int size = 0;
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nStack is empty");
+                return;
+            }
+            Console.Write("\nStack Elements => ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+                size++;
+            }
+            Console.WriteLine("\nTotal Elements in Stack: " + size);
+        }
+    }
+}
